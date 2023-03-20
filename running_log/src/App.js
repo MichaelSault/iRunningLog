4ced957d55ec1dd1b76315425f2faa9ec0d201bc
MichaelSault <michael_sault@hotmail.com>
2023-03-19 22:22:01 -0400

    built basic structure of front end

---
+++ running_log/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, {useState} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './component/login.component';
+import SignUp from './component/signup.component';
+import LogRun from './component/logRun.component';
+import ViewRuns from './component/viewRuns.component';
+import Progression from './component/progression.component';
+
+import MenuAppBar from './component/menuBar.component';
+
+function App() {
+
+  return (
+    <>
+    <MenuAppBar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/Login' element={<Login />}/>
+        <Route path='/SignUp' element={<SignUp />}/>
+        <Route path='/LogRun' element={<LogRun />}/>
+        <Route path='/ViewRuns' element={<ViewRuns />}/>
+        <Route path='/Progress' element={<Progression />}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ running_log/src/component/login.component.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react';
+import '../App.css';
+import Button from '@mui/material/Button';
+
+
+export default function LogIn() {
+    
+    return (
+        <div className="App">
+        <header className="App-header">
+            
+        </header>
+        </div>
+    );
+}
